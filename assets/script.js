1251e9410dc3fd88e8a09294a94ea3ac64afe9c9
unknown <kawecezarjesus@gmail.com>
2025-06-30 18:25:19 -0500

    updating the code, fixing the cast button, addind addeventlistener

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 const hamburger = document.querySelector(".hamburger")
 const nav = document.querySelector(".nav")
+const the_cast_btn = document.querySelector("#the-cast-btn")
+const contentmain = document.querySelector("#contentmain")
+const cast = document.querySelector("#cast")
+
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"))
 
@@ -14,10 +18,7 @@ function tamanho() {
     }
 }
 
-function cast() {
-    if(contentmain.style.display == 'none') {
-        contentmain.style.display = 'block'
-    } else {
-        contentmain.style.display ='none'
-    }
-}
+the_cast_btn.addEventListener("click", () => {
+    contentmain.classList.toggle("the-cast-act")
+    cast.classList.toggle("cast-act")
+})
